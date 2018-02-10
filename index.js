685534eef44e32428ee92628a188bb691a47fa20
Yuji Takaesu <taka.esu.user@gmail.com>
2018-02-11 07:00:59 +0900

    add npm packages color and prettier

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-console.log('Hello World')
-console.log('Hello World')
-console.log('Hello World')
-console.log('Hello World')
+const color = require('color')
+
+const redHexa = color({ r: 255, g: 0, b: 0 }).hex()
+
+console.log(redHexa);
