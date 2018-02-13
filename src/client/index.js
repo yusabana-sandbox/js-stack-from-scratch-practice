2e4795d0f32bd16ecfc7295e243038d932505404
Yuji Takaesu <taka.esu.user@gmail.com>
2018-02-14 08:04:48 +0900

    chap4: Setup webpack

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,4 @@ import 'babel-polyfill'
 
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 
-document.querySelector(APP_CONTAINER_SELECTOR).innerHTML = '<h1>Hello Webpack!</h1>'
+document.querySelector(APP_CONTAINER_SELECTOR).innerHTML = '<h1>Hello Webpack!!!!!!!!!!</h1>'
