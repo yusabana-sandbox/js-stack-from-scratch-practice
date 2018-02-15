2c20c39a68c4c5d5b94d87390c2a756966330927
Yuji Takaesu <taka.esu.user@gmail.com>
2018-02-15 18:58:34 +0900

    chap6: Add nav using react-router

---
+++ src/client/component/nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {
+  HOME_PAGE_ROUTE,
+  HELLO_PAGE_ROUTE,
+  HELLO_ASYNC_PAGE_ROUTE,
+  NOT_FOUND_DEMO_PAGE_ROUTE,
+} from '../../shared/routes'
+
+const Nav = () => (
+  <nav>
+    <ur>
+      {[
+        { route: HOME_PAGE_ROUTE, label: 'Home' },
+        { route: HELLO_PAGE_ROUTE, label: 'Say Hello' },
+        { route: HELLO_ASYNC_PAGE_ROUTE, label: 'Say Hello Asynchronously' },
+        { route: NOT_FOUND_DEMO_PAGE_ROUTE, label: '404 Demo' },
+      ].map(link => (
+        <li key={link.route}>
+          <NavLink to={link.route} activeStyle={{ color: 'red' }} exact>
+            {link.label}
+          </NavLink>
+        </li>
+      ))}
+    </ur>
+  </nav>
+)
+
+export default Nav
--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,7 +1,11 @@
 // @flow
 
-// eslint-disable-next-line import/prefer-default-export
-export const helloEndpointRoute = (num: ?number) => `/ajax/hello/${num || ':num'}`
+export const HOME_PAGE_ROUTE = '/'
+export const HELLO_PAGE_ROUTE = '/hello'
+export const HELLO_ASYNC_PAGE_ROUTE = '/hello-async'
+export const NOT_FOUND_DEMO_PAGE_ROUTE = '/404'
+
 // 以下のようなイメージ Express と 実際のアクセス
 // helloEndpointRoute()     // -> '/ajax/hello/:num' (for Express)
 // helloEndpointRoute(1234) // -> '/ajax/hello/1234' (for the actual call)
+export const helloEndpointRoute = (num: ?number) => `/ajax/hello/${num || ':num'}`
